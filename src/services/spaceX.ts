d49a26b2ba61bcdbf3c6b807439db16b5cdb5e55
atcamo <atiliocm@gmail.com>
2023-11-18 03:18:27 -0300

    listo a merge

---
+++ src/services/spaceX.ts
@@ -0,0 +1,30 @@
+import { type Launch, type SpaceXResponse } from '../types/SpaceXResponse';
+
+export const getLaunches = async (sort = 'desc') => {
+  const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: sort,
+        },
+        limit: 10,
+      },
+    }),
+  });
+  const { docs: launches } = (await res.json()) as SpaceXResponse;
+  return launches;
+};
+
+export const getLaunchById = async (id: string) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+  if (!res.ok) {
+    return {};
+  }
+  const launch = (await res.json()) as Launch;
+  return launch;
+};
